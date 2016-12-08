a0f9c7a09556a53f877e773f73f1baed328a77e7
crirodrirb28 <cristianrb28@hotmail.com>
2016-12-07 21:27:06 -0600

    Inicio del código final.
    Implementación del codigo base para el proyecto final.

---
+++ Codigo_Proyecto_Final/cubo_leds/cubo_leds.ino
@@ -0,0 +1,24 @@
+void setup() {
+pinMode( 30, OUTPUT );pinMode( 31, OUTPUT );pinMode( 32, OUTPUT );pinMode( 33, OUTPUT );pinMode( 34, OUTPUT );pinMode( 35, OUTPUT );pinMode( 36, OUTPUT );
+pinMode( 37, OUTPUT );pinMode( 38, OUTPUT );pinMode( 39, OUTPUT );pinMode( 40, OUTPUT );pinMode( 41, OUTPUT );pinMode( 42, OUTPUT );pinMode( 43, OUTPUT );
+pinMode( 44, OUTPUT );pinMode( 45, OUTPUT );pinMode( 46, OUTPUT );pinMode( 47, OUTPUT );pinMode( 48, OUTPUT );pinMode( 49, OUTPUT );pinMode(2,OUTPUT);
+Serial.begin(9600);
+}
+//a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p = para indicar cuales anodos del cubo vamos a encender o apagar con => HIGH o LOW
+//q, r, s, t  = para indicar cuales catodos del cubo vamos a encender o apagar con => HIGH o LOW
+//30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 = son los pines que sescogimos para usar en el ARDUINO MEGA.
+void PrenderLeds(boolean a, boolean b, boolean c, boolean d, boolean e, boolean f, boolean g, boolean h, boolean i, boolean j, boolean k, boolean l,boolean m,
+boolean n, boolean o, boolean p, boolean q, boolean r, boolean s, boolean t){
+   digitalWrite( 30, a );digitalWrite( 31, b);digitalWrite( 32, c );digitalWrite( 33, d);digitalWrite( 34, e );digitalWrite( 35, f );
+   digitalWrite( 36, g );digitalWrite( 37, h );digitalWrite( 38, i );digitalWrite( 39, j );digitalWrite( 40, k );digitalWrite( 41, l );
+   digitalWrite( 42, m);digitalWrite( 43, n );digitalWrite( 44, o );digitalWrite( 45, p );
+   
+   digitalWrite( 46, q );digitalWrite( 47, r );digitalWrite( 48, s );digitalWrite( 49,t);
+   delay(1);
+}
+ 
+ 
+ void loop() {
+  PrenderLeds(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
+ }
+
